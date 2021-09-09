9267dc3870241392d39c0f02a456520e57050cde
Yojahuang <yojahuang30613@gmail.com>
2021-09-09 22:34:18 +0800

    Add placeholder and rewrite the variable with ref method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,21 +5,17 @@
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
-import {reactive} from "vue"
 import {useCookie} from 'vue-cookie-next'
 import {useRoute}  from 'vue-router'
+import {ref, reactive} from "vue"
 
 export default {
   name: 'Home',
   setup() {
       let route = useRoute()
-      const state = reactive({
-          code : route.query.code
-      })
       const {setCookie} = useCookie()
       setCookie("code", route.query.code)
-      // this.$cookies.set("code", route.query.code)
-      return {state}
+      console.log(route.query.code)
   }
 }
 </script>
--- src/views/domainRegister.vue
+++ src/views/domainRegister.vue
@@ -2,7 +2,7 @@
     <div class="text-center">
         <div class="text-md lg:text-2xl mb-4">請於下欄中輸入您想註冊的網域<br />系統會為您確認是否可註冊</div>
         <div class="text-md md:text-3xl lg:text-4xl font-bold mb-4">
-            www. <input class="bg-gray-300 rounded-2xl" type="text" v-model.trim="data.code"> .nycu.me
+            www. <input class="bg-gray-300 rounded-2xl" type="text" v-model.trim="code" placeholder="英文字母或數字或符號"> .nycu.me
         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="sent()">
           送出
@@ -12,19 +12,18 @@
 
 <script>
 // @ is an alias to /src
-import {reactive} from "vue"
+import {ref, reactive} from "vue"
+
 
 export default {
   name: 'domainRegister',
   setup() {
-      const data = reactive({
-          code : ""
-      })
-      const sent = function() {
-          console.log(data.code);
+      const code = ref("")
+      const sent = async function() {
+          console.log(code.value);
       }
 
-      return {data, sent}
+      return {code, sent}
   }
 }
 </script>
