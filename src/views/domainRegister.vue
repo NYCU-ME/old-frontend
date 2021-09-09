101374eef23a9dbf7b0e6150d55d891d0841227d
Yojahuang <yojahuang30613@gmail.com>
2021-09-09 15:24:37 +0800

    Circular input field

--- src/views/domainRegister.vue
+++ src/views/domainRegister.vue
@@ -2,7 +2,7 @@
     <div class="text-center">
         <div class="text-md md:text-2xl mb-4">請於下欄中輸入您想註冊的網域<br />系統會為您確認是否可註冊</div>
         <div class="text-md md:text-4xl font-bold">
-            www. <input type="text"> .nycu.me
+            www. <input class="bg-gray-300 rounded-2xl" type="text"> .nycu.me
         </div>
     </div>
 </template>
