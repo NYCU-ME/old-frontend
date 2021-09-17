7c1ab77cbe3de4209195defcea5fee49d0a65575
Yojahuang <yojahuang30613@gmail.com>
2021-09-17 19:41:14 +0800

    Update domain register page

--- src/views/domainRegister.vue
+++ src/views/domainRegister.vue
@@ -2,7 +2,7 @@
     <div class="text-center">
         <div class="text-md lg:text-2xl mb-4">請於下欄中輸入您想註冊的網域<br />系統會為您確認是否可註冊</div>
         <div class="text-md md:text-3xl lg:text-4xl font-bold mb-4">
-            www. <input class="bg-gray-300 rounded-2xl" type="text" v-model.trim="customUrl" placeholder="英文字母或數字或符號"> .nycu.me
+            <input class="bg-gray-300 rounded-2xl" type="text" v-model.trim="customUrl" placeholder="英文字母或數字或符號"> .nycu.me
         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="sent()">
           送出
