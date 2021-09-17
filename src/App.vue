4bba110d31a39d07c32a964c4e08b757426bbda5
Yojahuang <yojahuang30613@gmail.com>
2021-09-17 20:46:25 +0800

    Navbar beautify

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="bg-indigo-500 shadow w-full md:py-3 pt-3 md:text-2xl text-white">
-        <div class="flex justify-between">
+    <div id="navbar" class="shadow w-full md:py-3 pt-3 pb-3 md:text-xl text-gray-400">
+        <div class="flex justify-between items-center">
             <div class="md:flex">
-                <router-link class="ml-3 md:mr-3 lg:py-1" to="/">NYCU.ME</router-link>
+                <router-link class="ml-6 text-2xl text-white align-middle md:mr-3 lg:py-1" to="/">NYCU.ME</router-link>
             </div>
             <div class="md:flex hidden">
                 <router-link class="md:ml-3 lg:py-1" to="/domainManage">網域管理</router-link>
@@ -12,7 +12,7 @@
             </div>
 
             <div class="md:hidden flex mr-3">
-                <button @click="showMobileMenu()">
+                <button @click="showMobileMenu()" class="border border-gray-400 p-1">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
                     </svg>
@@ -21,11 +21,11 @@
             </div>
         </div>
 
-        <div class="bg-gray-300 mt-2 bg-indigo-500 text-white md:hidden" :class="{hidden: isHidden}">
-            <router-link v-if="isLogin()" class="block py-2 text-center border" to="/logout">登出</router-link>
-            <router-link v-else class="block py-2 text-center border" to="/login">登入</router-link>
-            <router-link class="block py-2 text-center border" to="/domainManage">網域管理</router-link>
-            <router-link class="block py-2 text-center border" to="/domainRegister">網域註冊</router-link>
+        <div class="text-left md:hidden mt-3 ml-6" :class="{hidden: isHidden}">
+            <router-link v-if="isLogin()" class="block py-2" to="/logout">登出</router-link>
+            <router-link v-else class="block py-2" to="/login">登入</router-link>
+            <router-link class="block py-2" to="/domainManage">網域管理</router-link>
+            <router-link class="block py-2" to="/domainRegister">網域註冊</router-link>
         </div>
     </div>
 
@@ -63,6 +63,10 @@ export default {
 
 <style>
 a.router-link-exact-active {
-    font-weight: bold;
+    color: white;
+}
+
+#navbar {
+    background-color: #343a40;
 }
 </style>
--- src/views/domainRegister.vue
+++ src/views/domainRegister.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="text-center">
         <div class="text-md lg:text-2xl mb-4">請於下欄中輸入您想註冊的網域<br />系統會為您確認是否可註冊</div>
-        <div class="text-md md:text-3xl lg:text-4xl font-bold mb-4">
-            <input class="bg-gray-300 rounded-2xl" type="text" v-model.trim="customUrl" placeholder="英文字母或數字或符號"> .nycu.me
+        <div class="text-md md:text-2xl lg:text-3xl font-bold mb-4">
+            <input class="bg-gray-300" type="text" v-model.trim="customUrl" placeholder=" 英文字母或數字或符號"> .nycu.me
         </div>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="sent()">
           送出
@@ -27,3 +27,9 @@ export default {
   }
 }
 </script>
+
+<style>
+input::-webkit-input-placeholder {
+    font-size: 22px;
+}
+</style>
