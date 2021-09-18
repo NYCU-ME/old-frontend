a19a55c0f3a8cb330f579544e66505e73d2ae056
Yojahuang <yojahuang30613@gmail.com>
2021-09-18 16:12:46 +0800

    Add domainManage components

---
+++ src/components/domainManage.vue
@@ -0,0 +1,15 @@
+<template>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: 'domainManage',
+    props: ['domains'],
+    setup() {
+    }
+}
+</script>
