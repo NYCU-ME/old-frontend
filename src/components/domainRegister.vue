395594addcdd069149d7e3427174558860e428e8
Yojahuang <yojahuang30613@gmail.com>
2022-01-06 21:48:03 +0800

    Change domainRegister into a single button with + symbol

--- src/components/domainRegister.vue
+++ src/components/domainRegister.vue
@@ -1,4 +1,12 @@
 <template>
+    <div class="absolute top-13 left-0">
+        <button class="bg-blue-500 hover:bg-blue-700 m-2 text-white font-bold py-2 px-2 rounded-full">
+            <img src="@/assets/plus-solid.svg" class="align-middle inline" width="20" height="20"/> &nbsp;<div class="align-middle inline">註冊新網域</div>
+        </button>
+    </div>
+
+
+    <!--
     <div class="text-center">
         <div class="text-md lg:text-2xl mb-4">請於下欄中輸入您想註冊的網域<br />系統會為您確認是否可註冊</div>
         <div class="text-md md:text-2xl lg:text-3xl font-bold mb-4">
@@ -8,6 +16,7 @@
           送出
         </button>
     </div>
+    -->
 </template>
 
 <script>
@@ -44,6 +53,7 @@ export default {
                   console.log(statusCode)
                   if (statusCode == 401) {
                       cookie.removeCookie("token")
+                      alert("Not logged\n")
                       location.replace(config.getHomepageURL())
                   } else if (statusCode == 403) {
                       alert(error.response.data.msg)
