94d1201f3bd0f1445e6f4a3b9883cc6791baab88
Yojahuang <yojahuang30613@gmail.com>
2022-01-15 23:45:46 +0800

    Finish add record, delete record feature

--- src/components/domainManage.vue
+++ src/components/domainManage.vue
@@ -30,21 +30,11 @@
                                         <td class="px-6 py-3">{{record["type"]}}</td>
                                         <td class="px-6 py-3">{{record["value"]}}</td>
                                         <td class="px-6 py-3">{{record["ttl"]}}</td>
-                                        <td class="px-6 py-3"><a href="#">刪除紀錄</a></td>
+                                        <td class="px-6 py-3">
+                                            <img src="@/assets/trash-alt-solid.svg" class="align-middle inline mx-auto" width="25" height="25" @click="deleteRecord(record['type'], record['value'])"/>
+                                        </td>
                                     </tr>
                                 </template>
-                                <tr>
-                                    <td class="px-6 py-3">A</td>
-                                    <td class="px-6 py-3">140.113.127.5</td>
-                                    <td class="px-6 py-3">3</td>
-                                    <td class="px-6 py-3"><a href="#">刪除紀錄</a></td>
-                                </tr>
-                                <tr>
-                                    <td class="px-6 py-3">A</td>
-                                    <td class="px-6 py-3">140.113.127.5</td>
-                                    <td class="px-6 py-3">3</td>
-                                    <td class="px-6 py-3"><a href="#">刪除紀錄</a></td>
-                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -61,7 +51,7 @@
                 <div class="bg-white text-center p-4">
                     <div class="w-3/4 border-2 m-auto p-3 border-green-600">
                         <p class="text-lg">1. 請選擇紀錄的種類</p>
-                        <select class="bg-white border-2 rounded-lg m-2 w-3/4 md:w-1/2">
+                        <select v-model="recordType" class="bg-white border-2 rounded-lg m-2 w-3/4 md:w-1/2">
                             <option value="A">A</option>
                             <option value="AAAA">AAAA</option>
                             <option value="CNAME">CNAME</option>
@@ -74,28 +64,27 @@
                 <div class="bg-white text-center p-4">
                     <div class="w-3/4 border-2 m-auto p-3 border-green-600">
                         2. 內容<br />
-                        <input class="text-black bg-white border-2 p-1 w-3/4 overflow-hidden"/> <br />
+                        <input class="text-black bg-white border-2 p-1 w-3/4 overflow-hidden" v-model.trim="recordValue" /> <br />
                     </div>
                 </div>
 
                 <div class="bg-white text-center p-4">
                     <div class="w-3/4 border-2 m-auto p-3 border-green-600">
                         3. TTL<br />
-                        <input class="text-black bg-white border-2 p-1 w-3/4 overflow-auto" placeholder="選填"/> <br />
-                        <span>請填 [0, 86400] 之間的數字, 留空的話會自動處理</span>
+                        <input class="text-black bg-white border-2 p-1 w-3/4 overflow-auto" placeholder="選填" v-model.trim="recordTTL"/> <br />
+                        <span>請填 [1, 86400] 之間的數字<br /> 留空或不合法輸入的話會自動變成 5</span>
                     </div>
                 </div>
 
                 <div class="flex flex-wrap w-full bg-white rounded-b-lg px-2 py-2">
-                    <button class="text-white py-1 px-2 block bg-blue-500 hover:bg-blue-700 m-auto">送出</button>
-                    <button class="text-black py-1 px-2 block hover:bg-red-500 m-auto" @click="recordForm = !recordForm">關閉</button>
+                    <button class="text-white py-1 px-2 block bg-blue-500 hover:bg-blue-700 m-auto" @click="addRecord">送出</button>
+                    <button class="text-black py-1 px-2 block hover:bg-red-500 m-auto" @click="toggleRecord">關閉</button>
                 </div>
             </div>
         </transition>
     </div>
 
-    <div v-if="recordForm" @click="recordForm = !recordForm" class="opacity-50 bg-gray-600 absolute inset-0 w-full"></div>
-
+    <div v-if="recordForm" @click="toggleRecord" class="opacity-50 bg-gray-600 absolute inset-0 w-full"></div>
 
     <div v-if="!settingForm()" class="flex flex-col w-3/4 md:w-5/12 mx-auto mt-20">
         <div class="-my-2 overflow-x-auto -mx-6 md:-mx-8">
@@ -125,10 +114,10 @@
                                     <td class="px-4 py-3 whitespace-nowrap align-middle text-md text-black font-medium">{{domain["domainName"]}}</td>
                                     <td class="px-4 py-3 whitespace-nowrap align-middle text-md text-black">{{domain["expDate"]}}</td>
                                     <td class="px-4 py-3 whitespace-nowrap text-md font-medium">
-                                        <img src="@/assets/setting.png" class="align-middle inline mx-auto" width="25" height="25" @click="redirect(domain['id'])"/>
+                                        <img src="@/assets/cog-solid.svg" class="align-middle inline mx-auto" width="25" height="25" @click="redirect(domain['id'])"/>
                                     </td>
                                     <td class="px-4 py-3 whitespace-nowrap text-md font-medium">
-                                        <img src="@/assets/delete.png" class="align-middle inline mx-auto" width="25" height="25" @click="releaseDomain(domain['id'])"/>
+                                        <img src="@/assets/trash-alt-solid.svg" class="align-middle inline mx-auto" width="25" height="25" @click="releaseDomain(domain['id'])"/>
                                     </td>
                                     <td class="px-4 py-3 whitespace-nowrap text-md font-medium">
                                         <img v-if="needRenew(domain['id'])" @click="renew(domain['id'])" src="@/assets/renew.png" class="mx-auto align-middle inline" width="30" height="30"/>
@@ -158,6 +147,10 @@ export default {
     name: 'domainManage',
     async setup() {
         const route = useRoute()
+
+        let recordTTL = ref(null)
+        let recordValue = ref(null)
+        let recordType = ref(null)
 
         let recordForm = ref(false)
         let records = ""
@@ -273,7 +266,7 @@ export default {
                         let result = await axios.post(currentURL, {}, requestConfig)
                         console.log(result)
                         location.replace(config.getHomepageURL() + "/domain")
-                    } catch(error) {
+                    } catch (error) {
                         if (error.response) {
                             let statusCode = error.response.status
                             console.log(statusCode)
@@ -290,7 +283,85 @@ export default {
             }
         }
 
-        return {domains, needRenew, renew, redirect, releaseDomain, records, settingForm, recordForm}
+        // toggle add record dialog
+        function toggleRecord() {
+            recordForm.value = !recordForm.value
+            recordTTL.value = null
+            recordValue.value = null
+            recordType.value = null
+        }
+
+        // add record
+        async function addRecord() {
+            let currentURL = baseURL + "/ddns"
+            let domainNames = ""
+
+            for (let i = 0; i < domains.length; ++i)
+                if (domains[i]["id"] == route.query.idx) domainNames = getDomainNames(domains[i]["domainName"])
+
+            for (let i = 0; i < domainNames.length; ++i)
+                currentURL = currentURL + "/" + domainNames[i]
+
+            currentURL = currentURL + "/records/" + recordType.value + "/" + recordValue.value
+
+            let requestData = {
+
+            }
+            if (recordTTL.value != null) {
+                requestData["ttl"] = recordTTL.value
+            }
+
+            try {
+                let result = await axios.post(currentURL, requestData, requestConfig)
+                console.log(result)
+                redirect(route.query.idx)
+            } catch (error) {
+                if (error.response) {
+                    let statusCode = error.response.status
+                    console.log(statusCode)
+                    if (statusCode == 401) {
+                        cookie.removeCookie("token")
+                        alert("Not logged\n")
+                        location.replace(config.getHomepageURL())
+                    } else if (statusCode == 403) {
+                        alert(error.response.data.msg)
+                    }
+                }
+            }
+        }
+
+        async function deleteRecord(type, value) {
+            let currentURL = baseURL + "/ddns"
+            let domainNames = ""
+
+            for (let i = 0; i < domains.length; ++i)
+                if (domains[i]["id"] == route.query.idx) domainNames = getDomainNames(domains[i]["domainName"])
+
+            for (let i = 0; i < domainNames.length; ++i)
+                currentURL = currentURL + "/" + domainNames[i]
+
+            currentURL = currentURL + "/records/" + type + "/" + value
+
+            try {
+                let result = await axios.delete(currentURL, requestConfig)
+                console.log(result)
+                redirect(route.query.idx)
+            } catch (error) {
+                if (error.response) {
+                    let statusCode = error.response.status
+                    console.log(statusCode)
+                    if (statusCode == 401) {
+                        cookie.removeCookie("token")
+                        alert("Not logged\n")
+                        location.replace(config.getHomepageURL())
+                    } else if (statusCode == 403) {
+                        alert(error.response.data.msg)
+                    }
+                }
+            }
+        }
+
+        return {domains, needRenew, renew, redirect, releaseDomain, records, settingForm, recordForm, recordTTL, recordValue, addRecord, recordType, toggleRecord, deleteRecord}
     }
 }
 </script>
