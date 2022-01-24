0e7f8ce1d95e296cbe1960069ebb7ad6863ebb72
Yojahuang <yojahuang30613@gmail.com>
2022-01-24 16:35:39 +0800

    Add wdzeng (sponsor)

--- src/views/about.vue
+++ src/views/about.vue
@@ -26,6 +26,15 @@
                 <p class="m-2 align-middle inline">https://linlee.cat</p>
             </a>
         </div>
+
+        <div class="mx-auto my-2 border-4 text-center p-2 border-black rounded-xl">
+            <img class="mx-auto" src="@/assets/contact/wdzeng.jpeg" width="280" />
+            <div class="text-3xl font-medium">雙曲線</div>
+            <div class="bg-red-600 mx-auto text-white font-bold rounded-xl w-min py-1 px-2">Sponsor</div>
+            <a class="text-lg" href="https://github.com/wdzeng">
+                <p class="m-2 align-middle inline">https://github.com/wdzeng</p>
+            </a>
+        </div>
     </template>
 
 </template>
