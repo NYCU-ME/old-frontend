06230106a0902c1ccb38cfd6d3d2cf76143cdbea
Hyperbola <me@hyperbola.me>
2022-02-26 06:18:50 +0800

    update about page css

--- src/views/about.vue
+++ src/views/about.vue
@@ -1,60 +1,60 @@
 <template>
     <div class="container mx-auto py-24 flex justify-center">
         <div class="grid gap-y-16 gap-x-8 grid-cols-[fit-content(100%)] md:grid-cols-[fit-content(100%)_fit-content(100%)] lg:grid-cols-[fit-content(100%)_fit-content(100%)_fit-content(100%)]">
-            <div class="py-8 px-4 border rounded shadow-xl aspect-[3/4] flex items-center">
+            <div class="about-card">
                 <div>
-                    <div class="flex justify-center mb-4 shadow-xl">
-                        <img class src="@/assets/contact/yojahuang.jpg" width="280" />
+                    <div class="profile-image-wrapper">
+                        <img src="@/assets/contact/yojahuang.jpg" />
                     </div>
-                    <h2 class="text-3xl font-medium text-center mb-4">Yoja Huang</h2>
-                    <p class="flex justify-center items-stretch">
-                        <span class="inline-flex justify-center w-24 bg-blue-500 rounded text-white mr-2 cursor-pointer">Frontend</span>
-                        <a class="inline-flex items-center text-sm text-gray-700 hover:text-black" href="https://github.com/Yojahuang" target="_blank" rel="noopener noreferrer">
+                    <h2 class="profile-name">Yoja Huang</h2>
+                    <p class="intro-wrapper">
+                        <span class="badge bg-blue-500">Frontend</span>
+                        <a class="link" href="https://github.com/Yojahuang" target="_blank" rel="noopener noreferrer">
                             <fa icon="link" />
                         </a>
                     </p>
                 </div>
             </div>
 
-            <div class="py-8 px-4 border rounded shadow-xl aspect-[3/4] flex items-center">
+            <div class="about-card">
                 <div>
-                    <div class="flex justify-center mb-4 shadow-xl">
-                        <img class src="@/assets/contact/SiriusKoan.jpg" width="280" />
+                    <div class="profile-image-wrapper">
+                        <img src="@/assets/contact/SiriusKoan.jpg" />
                     </div>
-                    <h2 class="text-3xl font-medium text-center mb-4">Sirius Koan</h2>
-                    <p class="flex justify-center items-stretch">
-                        <span class="inline-flex justify-center w-24 bg-blue-500 rounded text-white mr-2 cursor-pointer">DevOps</span>
-                        <a class="inline-flex items-center text-sm text-gray-700 hover:text-black" href="https://github.com/SiriusKoan" target="_blank" rel="noopener noreferrer">
+                    <h2 class="profile-name">Sirius Koan</h2>
+                    <p class="intro-wrapper">
+                        <span class="badge bg-blue-500">DevOps</span>
+                        <a class="link" href="https://github.com/SiriusKoan" target="_blank" rel="noopener noreferrer">
                             <fa icon="link" />
                         </a>
                     </p>
                 </div>
             </div>
 
-            <div class="py-8 px-4 border rounded shadow-xl aspect-[3/4] flex items-center">
+            <div class="about-card">
                 <div>
-                    <div class="flex justify-center mb-4 shadow-xl">
-                        <img class src="@/assets/contact/linlee.jpeg" width="280" />
+                    <div class="profile-image-wrapper">
+                        <img src="@/assets/contact/linlee.jpeg" />
                     </div>
-                    <h2 class="text-3xl font-medium text-center mb-4">Lin Lee</h2>
-                    <p class="flex justify-center items-stretch">
-                        <span class="inline-flex justify-center w-24 bg-blue-500 rounded text-white mr-2 cursor-pointer">Backend</span>
-                        <a class="inline-flex items-center text-sm text-gray-700 hover:text-black" href="https://linlee.cat" target="_blank" rel="noopener noreferrer">
+                    <h2 class="profile-name">Lin Lee</h2>
+                    <p class="intro-wrapper">
+                        <span class="badge bg-blue-500">Backend</span>
+                        <a class="link" href="https://linlee.cat" target="_blank" rel="noopener noreferrer">
                             <fa icon="link" />
                         </a>
                     </p>
                 </div>
             </div>
 
-            <div class="py-8 px-4 border rounded shadow-xl aspect-[3/4] flex items-center">
+            <div class="about-card">
                 <div>
-                    <div class="flex justify-center mb-4 shadow-xl">
-                        <img class src="@/assets/contact/wdzeng.jpeg" width="280" />
+                    <div class="profile-image-wrapper">
+                        <img src="@/assets/contact/wdzeng.jpeg" />
                     </div>
-                    <h2 class="text-3xl font-medium text-center mb-4">Hyperbola</h2>
-                    <p class="flex justify-center items-stretch">
-                        <span class="inline-flex justify-center w-24 bg-red-500 rounded text-white mr-2 cursor-pointer">Sponsor</span>
-                        <a class="inline-flex items-center text-sm text-gray-700 hover:text-black" href="https://github.com/wdzeng" target="_blank" rel="noopener noreferrer">
+                    <h2 class="profile-name">Hyperbola</h2>
+                    <p class="intro-wrapper">
+                        <span class="badge bg-red-500">Sponsor</span>
+                        <a class="link" href="https://github.com/wdzeng" target="_blank" rel="noopener noreferrer">
                             <fa icon="link" />
                         </a>
                     </p>
@@ -74,3 +74,33 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.about-card {
+    @apply py-8 px-4 border rounded shadow-xl aspect-[3/4] flex items-center;
+
+    .profile-image-wrapper {
+        @apply flex justify-center mb-4 shadow-xl;
+
+        img {
+            @apply w-[280px] h-[280px] object-cover;
+        }
+    }
+
+    .profile-name {
+        @apply text-3xl font-medium text-center mb-4;
+    }
+
+    .intro-wrapper {
+        @apply flex justify-center items-stretch;
+
+        .badge {
+            @apply inline-flex justify-center w-24 rounded text-white mr-2 cursor-pointer;
+        }
+
+        .link {
+            @apply inline-flex items-center text-sm text-gray-700 hover:text-black;
+        }
+    }
+}
+</style>
